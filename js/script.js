4f7c1208c1c848ef84c75a7d7cd15d67a8d5bd36
Prisoner1992 <abiyevseymur92@gmail.com>
2017-03-16 07:00:31 +0400

    SlideShow

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+var n = 0;
+var left = document.getElementsByClassName('head')
+document.getElementsByClassName('button')[1].addEventListener('click', function (event) {
+    if (n == (document.getElementsByClassName('pic').length - 4) * 320) {
+        event.preventDefault();
+    }
+    else {
+        n = n + 320;
+        left[0].style.right = n + "px";
+    }
+})
+
+document.getElementsByClassName('button')[0].addEventListener('click', function () {
+    if (n > 0) {
+        n = n - 320;
+        left[0].style.right = n + "px"
+    }
+})
+
+
